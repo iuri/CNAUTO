400b80a52e548b9c939d232580708107b04eb230
Iuri Sampaio <iuri.sampaio@iurix.com>
2011-10-21 14:57:18 -0300

    fixed javascript slideshow.js IVC website

--- packages/theme-ivc/www/resources/js/slideshow.js
+++ packages/theme-ivc/www/resources/js/slideshow.js
@@ -5,8 +5,9 @@ wrapperid: "myslideshow", //ID of blank DIV on page to house Slideshow
 dimensions: [700, 300], //width/height of gallery in pixels. Should reflect dimensions of largest image
 imagearray: [
 	     ["/resources/theme-ivc/images/foto1.png"], //["image_path", "optional_link", "optional_target"]
-	     ["/resources/theme-ivc/images/foto1.png", "http://en.wikipedia.org/wiki/Cave", "_new"],
-	     ["sources/theme-ivc/images/foto1.png"] //<--no trailing comma after very last image element!
+	     ["/resources/theme-ivc/images/foto2.png", "http://en.wikipedia.org/wiki/Cave", "_new"],
+	     ["/resources/theme-ivc/images/foto1.png"], //<--no trailing comma after very last image element!
+	     ["/resources/theme-ivc/images/foto2.png"] //<--no trailing comma after very last image element!
 	     ],
 displaymode: {type:'auto', pause:2000, cycles:2, pauseonmouseover:true},
 orientation: "h", //Valid values: "h" or "v"
